ce9f8aa59d90b860b7fcc6d7c0ec01d46f2ab407
PeterQLee <leep1995@gmail.com>
2015-05-15 18:58:46 -0300

    DEL

--- Site/js/maccam.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    //$("#refresh").hide();
-    $(".eventlogs").load("logs/maccamLogs.txt");
-    $("#refresh").click(function() {
-        $(".eventlogs").load("logs/maccamLogs.txt");
-    });
-});
