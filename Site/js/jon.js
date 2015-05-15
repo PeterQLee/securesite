722851a216b21e7dfcf129e28400e5578f15d00b
Peter Lee <leep1995@gmail.com>
2015-05-15 13:58:25 -0300

    Delete jon.js

--- Site/js/jon.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    //$("#refresh").hide();
-    $(".eventlogs").load("logs/jonLogs.txt");
-    $("#refresh").click(function() {
-        $(".eventlogs").load("logs/jonLogs.txt");
-    });
-});
